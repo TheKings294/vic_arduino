01466d3f5ba6d6297d720cd44a1195499a2b7957
TheKings294 <98644094+TheKings294@users.noreply.github.com>
2025-04-11 18:23:46 +0200

    feat: add code

--- projet_s2.ino
+++ projet_s2.ino
@@ -1,21 +1,30 @@
 #include "kbAcy.h"
-#define BUZZER_PIN 8
-#define W 
-#define X 
-#define Y 
-#define Z 
+#include <Servo.h>
 
+#define BUZZER_PIN 8
+#define SERVO_MOTEUR_PIN 
+#define LIGHT_SENSOR_PIN A5
+#define LED_RED_PIN 
+#define LED_GREEN_PIN 
+
+const int SERVO_POS_DAY = 0
+const int SERVO_POS_NIGHT = 0
  
 void setup() {
-
+    //Definition of th rate of the serial monitor
     Serial.begin(9600);
 
-    pinMode(A5,INPUT);
-    pinMode(5,OUTPUT);
-    pinMode(7,OUTPUT);
-    pinMode(1,OUTPUT);
+    //Definiton of the pi mode
+    pinMode(LIGHT_SENSOR_PIN, INPUT); //Light sensor
+    pinMode(SERVO_MOTEUR_PIN, OUTPUT); //Servo moteur
+    pinMode(BUZZER_PIN, OUTPUT); //Buzzer
+    pinMode(LED_GREEN_PIN, OUTPUT); //Grenn LED
+    pinMode(LED_GREEN_PIN, OUTPUT); //Red LED
 
-    //int l_mescan = 0;
+    //Defintion of the servo motor
+    Servo light_servo;
+    light_servo.attach(SERVO_MOTEUR_PIN);
+
     float l_vmes = 0;
     float l_mes0;
     float l_var;
@@ -23,28 +32,26 @@ void setup() {
 
 void loop() {
     l_mes0 = l_vmes;
-    l_vmes = (analogRead(A5)/204.6);
+    l_vmes = (analogRead(LIGHT_SENSOR_PIN)/204.6);
     Serial.println(l_vmes);
     l_var = abs(l_mes0) - abs(l_vmes);
 
-    if(l_var > 1){
-        tone(1, 100, 2000);
-    }
-    else{
-        noTone(1);
-    }
-
-    if (l_vmes > 3.5){
-        setLight("Lumière !!")
-    }
-    else{
-        setLight("Obscurité !!");
-    }
+    if (abs(l_vmes - l_mes0) >= 2) {
+        l_var > 1 ? tone(1, 100, 2000) : noTone(1); //if l_var > at 1 tone else no tone
+        l_vmes > 3.5 ? setDayMode() : setNightMode(); //if l_vmes > at 3,5 setDayMode else setNihntMode
+    }    
 }
 
-void setLight(String valuePrint) {
-    Serial.println(valuePrint);
-    digitalWrite(7, HIGH);
-    delay(2000);
-    digitalWrite(5, LOW);
+void setDayMode() {
+    Serial.println("Day mode !!");
+    digitalWrite(LED_GREEN_PIN, HIGH);
+    light_servo.write(SERVO_POS_DAY);
+    digitalWrite(LED_RED_PIN, LOW);
+} 
+
+void setNightMode() {
+    Serial.println("Night Mode");
+    digitalWrite(LED_RED_PIN, HIGH);
+    light_servo.write(SERVO_POS_NIGHT);
+    digitalWrite(LED_GREEN_PIN, LOW);
 }
